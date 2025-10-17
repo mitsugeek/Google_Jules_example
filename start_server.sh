bf79400ce7ce1c120fad1f1d2717bfda3a2aed94
mitsugeek <mitsugeek@users.noreply.github.com>
2025-10-17 04:27:36 +0000

    deploy: 0cbb092d0cf91c8df044be5cfb2d6b2b83e7afcb

---
+++ start_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e # Exit immediately if a command exits with a non-zero status.
+
+# Configure bundler to use a local vendor path
+
+# Configure bundler to use a local vendor path
+bundle config set --local path 'vendor/bundle'
+
+# Install dependencies
+bundle install
+
+# Start the Jekyll server in the background
+bundle exec jekyll serve --host=0.0.0.0 > ../jekyll_output.log 2>&1 &
